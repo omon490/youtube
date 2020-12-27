3f3b8b5c19cbf12f023dc18c4225a7bd7bb804af
omon490 <omonbekmurodov@gmail.com>
2020-12-27 15:35:36 +0500

    Add recommend list

--- js/styles.js
+++ js/styles.js
@@ -3,9 +3,6 @@
   return node.querySelector(selector);
 };
 
-// var elIdish = $_('.idish');
-// var elButton = $_('button', elIdish);
-
 var $$_ = function (selector, node = document) {
   return node.querySelectorAll(selector);
 };
@@ -34,3 +31,117 @@ elMenuToggler.addEventListener(`click`, function () {
   // console.log(`ishladi`);
   elMenu.classList.toggle(`page-body--open`);
 });
+
+// Videolarga array ochamiz va ichiga obyektda ma'lumotlarni joylaymiz
+var elVideos = [
+  // obyekt ochamiz va vidoning nomi, rasmning joylashgan joyi, video ko'rilgan kun va necha marta ko'rilgani, vidoening avtori(bu yerda hamma videolar bir avtorga tegishli bo'lsa ham kiritib kettim sababi avtor o'zgarishi mumkin), va videoning davomiyligi
+  {
+    videoTitle: `A Brief History Of Creation`,
+    videoImage: `img/video-img-1/video-img-1.png`,
+    videoViews: `80k views  ·  3 days ago`,
+    videoAutor: `Dollie Blair`,
+    videoTime: `4:15`
+  },
+  {
+    videoTitle: `Selecting The Right Hotel`,
+    videoImage: `img/video-img-2/video-img-2.png`,
+    videoViews: `123k views  ·  1 months ago`,
+    videoAutor: `Dollie Blair`,
+    videoTime: `8:00`
+  },
+  {
+    videoTitle: `Asteroids`,
+    videoImage: `img/video-img-3/video-img-3.png`,
+    videoViews: `43k views  ·  12 days ago`,
+    videoAutor: `Dollie Blair`,
+    videoTime: `5:32`
+  },
+  {
+    videoTitle: `Telescopes 101`,
+    videoImage: `img/video-img-4/video-img-4.png`,
+    videoViews: `11k views  ·  6 months ago`,
+    videoAutor: `Dollie Blair`,
+    videoTime: `6:40`
+  },
+  {
+    videoTitle: `Medical Care Is Just`,
+    videoImage: `img/video-img-5/video-img-5.png`,
+    videoViews: `18k views  ·  2 days ago`,
+    videoAutor: `Dollie Blair`,
+    videoTime: `1:45`
+  },
+  {
+    videoTitle: `Moon Gazing`,
+    videoImage: `img/video-img-6/video-img-6.png`,
+    videoViews: `67k views  ·  4 months ago`,
+    videoAutor: `Dollie Blair`,
+    videoTime: `1:45`
+  },
+];
+
+var elRecommendVideo = [
+  {
+    videoRecTitle: `Dude You Re Getting A Telescope`,
+    videoRecImage: `img/recommend-img-1/recommend-img-1.png`,
+    videoRecViews: `34k views  ·  5 months ago`,
+    videoRecAutor: `Gussie French`,
+    videoRecTime: `3:40`
+  },
+  {
+    videoRecTitle: `Moon Gazing`,
+    videoRecImage: `img/recommend-img-2/recommend-img-2.png`,
+    videoRecViews: `54k views  ·  11 months ago`,
+    videoRecAutor: `Edward Osborne`,
+    videoRecTime: `2:12`
+  },
+  {
+    videoRecTitle: `Moon Gazing`,
+    videoRecImage: `img/recommend-img-3/recommend-img-3.png`,
+    videoRecViews: `125k views  ·  4 months ago`,
+    videoRecAutor: `Dollie Blair`,
+    videoRecTime: `2:12`
+  },
+];
+
+// elVideosList nomli o'zgaruvchi ochib uni documentdan topib olamiz
+var elVideosList = $_(`.js-videos-list-group`);
+var elRecommendList = $_(`.js-recommend-list`);
+// elVideosTemplate nomli o'zgaruvchiga templetening qayerda turganligini topib uni shunga tenglab qo'yamiz
+var elVideosTemplate = $_(`.site-content__videos-template`).content;
+var elRecommendTemplate = $_(`.site-content__recommend-template`).content;
+// elVideosFragment nomli fragment ochib olamiz bu bizga ma'lumotlarni bittada ko'rsatishga kerak bo'ladi
+var elVideosFragment = document.createDocumentFragment();
+var elRecommendFragment = document.createDocumentFragment();
+
+// endi elVideos nomli arrayni forEachda aylanib chiqamiz
+  elVideos.forEach(function (video) {
+    // elVideosItem nomli o'zgaruvchiga har biridan original qolipdan nusxa ko'chiramiz
+  var elVideosItem = elVideosTemplate.cloneNode(true);
+
+  // endi shu qolipning ichidan o'zgarishi kerak bo'lgan elementlarni topib olamiz, va ularning textContenti yoki o'zgarishi kerak bo'lgan atributlarini har bir arrayning ichidagi obyektlarga tenglab chiqamiz
+  $_(`.js-site-content__inner-img`, elVideosItem).src = video.videoImage;
+  $_(`.js-site-content__inner-time`, elVideosItem).textContent = video.videoTime;
+  $_(`.js-site-content__videos-title`, elVideosItem).textContent = video.videoTitle;
+  $_(`.js-site-content__info-views`, elVideosItem).textContent = video.videoViews;
+
+  // yuqoridagi fragmentga shu original qolipning nusxasini bolalari sifatida qo'shib qo'yamiz
+  elVideosFragment.appendChild(elVideosItem);
+});
+// va htmlda mavjud bo'lgan ulga fragmentni bolalari sifatida qo'shib qo'yamiz
+(elVideosList).appendChild(elVideosFragment);
+
+elRecommendVideo.forEach(function (recommend) {
+
+  var elRecommendItem = elRecommendTemplate.cloneNode(true);
+
+  $_(`.js-site-content__recommend-inner-img`, elRecommendItem).src = recommend.videoRecImage;
+  $_(`.js-site-content__recommend-inner-time`, elRecommendItem).textContent = recommend.videoRecTime;
+  $_(`.js-site-content__recommend-title`, elRecommendItem).textContent = recommend.videoRecTitle;
+  $_(`.js-site-content__recommend-info-views`, elRecommendItem).textContent = recommend.videoRecViews;
+
+  elRecommendFragment.appendChild(elRecommendItem);
+});
+(elRecommendList).appendChild(elRecommendFragment);
+
+
+
